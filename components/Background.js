290030a5f9ab7ce6c0edae0a47aa3d3b785a73fd
jacquelineturof <jacquelineturof@gmail.com>
2020-06-24 15:22:41 -0400

    changing layout

--- components/Background.js
+++ components/Background.js
@@ -5,6 +5,9 @@ import classes from './Background.module.css'
 const Background = () => (
     <div className = { classes.Background }>
         <img src = "sam1.png" alt = "sam" className = { classes.Sam } />
+
+        <h5 className = { classes.Heading }>Celebrity Shows</h5>
+        <p className = { classes.Info }>Electric Adventure 2014 + 2015, Victor Niglio, DVBBS, Bassjackers, Caked Up, 4B, DJ Snake, David S, DJ Yonny, Beatbreaker, GTA, Deorro, Fedde Le Grand, Markus Schulz, Dillion Francis, Corona (beer) + more.</p>
     </div>
 )
 
--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,6 @@ const Header = () => (
         <p className = { classes.Info }>Endless energy combined with a dynamic personality and million dollar smile has propelled Sam (BPN) into a sought after DJ/MC/Event Host. Sam began spinning records at a young age. At 14, she became a roadie for several New Jersey DJ companies where she was like a sponge, soaking up everything going on in order to learn and improve her skills. Originally from New York/New Jersey area where she grew up and fell in love with all types of music, now rocks parties on both coasts. In addition to being a private event DJ, Sam has opened, closed for, and worked with some of the biggest names in the music industry today. Excelling in both private parties and clubs, easy to work with, Sam’s originality transforms parties into a once in a lifetime magical event.</p>
         <h5 className = { classes.Heading }>Locations</h5>
         <p className = { classes.Info }> NJ, NYC, Staten Island, Long Island, Orlando, Tampa, Miami, Boca Raton, Seattle, Destination</p>
-        <h5 className = { classes.Heading }>Celebrity Shows</h5>
-        <p className = { classes.Info }>Electric Adventure 2014 + 2015, Victor Niglio, DVBBS, Bassjackers, Caked Up, 4B, DJ Snake, David S, DJ Yonny, Beatbreaker, GTA, Deorro, Fedde Le Grand, Markus Schulz, Dillion Francis, Corona (beer) + more.</p>
     </header>
 )
 
